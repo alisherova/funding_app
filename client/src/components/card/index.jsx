512af795b26e758947fe9ffcebffcb4c90cf967f
alisherova <alisherovanurjahon@gmail.com>
2023-07-22 03:31:53 +0500

    connecting front part with server

--- client/src/components/card/index.jsx
+++ client/src/components/card/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { CardWrapper, CardContent, CardImg, CardFooter } from './style'
+import { CardWrapper, CardContent, CardImg, CardFooter, Title, CompainWrapper, CompanyLogo, CompanyName, CardDateWrapper, CardDateSpan, GreenLine, DonatedSum, FundedSum } from './style'
 import Button from '../button'
 import { usePosts } from '../../hooks'
-
+import { FiUsers } from 'react-icons/fi'
+import { AiOutlineClockCircle } from 'react-icons/ai'
 const Card = () => {
 
   const { data, isLoading } = usePosts()
@@ -11,20 +12,45 @@ const Card = () => {
     return <p>Loading...</p>
   }
 
+  const getLeftDate = (lastDay) => {
+    let currentDate = new Date()
+    let splittedDate = lastDay.split('T')[0]
+    let pureDate = new Date(splittedDate)
+    const timeDifference = Math.abs(pureDate - currentDate);
+    const daysRemaining = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+    return daysRemaining;
+  }
+
+  const getLeftPercent = (funded, total) => {
+    let totalLiabilites = String(funded);
+    let toalAssets = String(total);
+    let pattern = /[^0-9.-]+/g;
+
+    let result = parseFloat(toalAssets.replace(pattern, '')) /
+      parseFloat(totalLiabilites.replace(pattern, ''));
+    return (Math.ceil(Number(result) * 100));
+  }
+
   return (
     data?.map(d => {
       return (<CardWrapper to={`/${d._id}`} key={d._id}>
-        <CardImg>
-          <img src="https://curiousworld-static-files.s3.amazonaws.com/blog/iStock-166637296.jpg" alt="donation img" />
-        </CardImg>
+        <CardImg src={d.mainImage} alt="donation img" />
         <CardContent>
-          <p className='title'>{d.title}</p>
-
+          <CompainWrapper>
+            <CompanyLogo src={d.company.logo} alt="donation img" />
+            <CompanyName>{d.company.name}</CompanyName>
+          </CompainWrapper>
+          <Title>{d.title}</Title>
+          <CardDateWrapper>
+            <FiUsers /><CardDateSpan>{d.supporters} </CardDateSpan>
+            <AiOutlineClockCircle /><CardDateSpan>{getLeftDate(d.lastDay)} days left</CardDateSpan>
+          </CardDateWrapper>
         </CardContent>
+        <GreenLine width={getLeftPercent(d.funded, d.total)} />
         <CardFooter>
           <div>
-            <p className="donationSum">${d.funded}</p>
-            <p className="neededSum">funded of ${d.total || 0}</p>
+            <DonatedSum className="donationSum">${d.funded}</DonatedSum>
+            <FundedSum className="neededSum">funded of ${d.total || 0}</FundedSum>
           </div>
           <Button />
         </CardFooter>
--- client/src/components/card/style.js
+++ client/src/components/card/style.js
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const CardWrapper = styled(Link)`
-  max-width: 300px;
+  max-width: 350px;
+  height: 500px;
   width: 30%;
-  background-color: #ffffff;
+  background-color: whitesmoke;
   border-radius: 12px;
-  border: solid 2px transparent;
   cursor: pointer;
   color: unset;
   text-decoration: none;
   &:hover {
-    border: solid 2px rgb(67, 196, 232);
+    scale: 1.02;
   }
 `;
 
@@ -19,12 +19,12 @@ export const CardContent = styled.div`
   padding: 10px;
 `;
 
-export const CardImg = styled.div`
-  img {
-    width: 100% !important;
-    border-top-left-radius: 12px;
-    border-top-right-radius: 12px;
-  }
+export const CardImg = styled.img`
+  width: 100%;
+  height: 45%;
+  object-fit: center;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
 `;
 
 export const CardFooter = styled.div`
@@ -32,12 +32,54 @@ export const CardFooter = styled.div`
   justify-content: space-between;
   align-items: center;
   margin: 10px;
+  margin-top: 1rem;
+`;
 
-  p:first-child {
-    font-weight: 600;
-    font-size: 12px;
-  }
-  p:last-child {
-    font-size: 10px;
-  }
+export const Title = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  padding: 0.7rem 0;
+`;
+export const CompainWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+`;
+
+export const CompanyLogo = styled.img`
+  width: 1.3rem;
+  height: 1.3rem;
+  border-radius: 50%;
+`;
+export const CompanyName = styled.p`
+  font-size: 10px;
+`;
+
+export const CardDateWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  color: gray;
+`;
+
+export const CardDateSpan = styled.span`
+  font-size: 10px;
+  margin-right: 1rem;
+`;
+
+export const GreenLine = styled.div`
+  width: ${(props) => props.width}%;
+  height: 3px;
+  background-color: green;
+  margin: 0.5rem 0;
+`;
+
+export const DonatedSum = styled.p`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const FundedSum = styled.p`
+  font-size: 10px;
+  color: gray;
 `;
